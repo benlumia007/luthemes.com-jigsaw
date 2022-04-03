759501148dfc83e01c9f190126011eba0e9acd7d
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-04-03 09:53:57 -0700

    initial commit

---
+++ public/_views/page-about.blade.php
@@ -0,0 +1,23 @@
+@extends( '_views.templates.main' )
+
+@section( 'body' )
+<section id="content" class="site-content">
+    <main class="content-area">
+        <article class="page">
+            <header class="entry-header">
+                <h1 class="entry-title">{{ $page->title }}</a></h1>
+            </header>
+            <div class="entry-content">
+                @yield( 'content' )
+            </div>
+        </article>
+    </main>
+    <aside id="aside" class="widget-area">
+        <div class="widget widget-thumbnail">
+            <figure>
+                <img src="{{ $page->thumbnail }}" />
+            </figure>
+        </div>
+    </aside>
+</section>
+@endsection
